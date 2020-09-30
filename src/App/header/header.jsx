1aef9f0fe76e777e7d0a6ea9b6b148469f678dd5
e-marketing AntonB <u1-278@office107.local>
2020-09-30 18:28:37 +0300

    finished header contacts

--- src/App/header/header.jsx
+++ src/App/header/header.jsx
@@ -4,16 +4,60 @@ const Header = () => {
     return (
         <header>
             <div className="inline-header">
-                <div className="col-md20">
+                <div className="col-w20">
                     <div className="logo-container">
                         <a className="logo" href="/"></a>
                     </div>
                 </div>
-                <div className="col-md80">
+                <div className="col-w80">
                     <div className="col-h50 contacts-container-main">
-                        <div className="contacts-container"></div>
+                        <div className="contacts-container">
+                            <div className="col-h50 top-contacts-container">
+                                <div className="contacts">
+                                    <div className="col-h50">
+                                        <div className="phones">
+                                            <p>+380501523665</p>
+                                            <p>+380978757141</p>
+                                            <p>+380509898041</p>
+                                        </div>
+                                    </div>
+                                    <div className="col-h50">
+                                        <div className="email">
+                                            <a href="https://mail.ukr.net/desktop#sendmsg">logistics-solutions@ukr.net</a>
+                                            <a href="https://www.facebook.com/companylogisticssolutions/">Facebook</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-h50 bottom-contacts-container"></div>
+                        </div>
                     </div>
-                    <div className="col-h50"></div>
+                    <div className="col-h50">
+                        <div className="navigations">
+                            <div className="navigation-container">
+                                <div className="col-h30"></div>
+                                <div className="col-h70 menu-container">
+                                    <div className="col-w25">
+                                        <div className="menu-buttons"><a href="#Main">Главная</a></div>
+                                    </div>
+                                    <div className="col-w25">
+                                        <div className="menu-buttons transport">
+                                            <a href="#Main">Транспорт</a>
+                                            {/* <div className="drop-menu">
+                                                <div className="menu-buttons"><a href="#Geography">География</a></div>
+                                                <div className="menu-buttons"><a href="#Transport">Автомобильные</a></div>
+                                                <div className="menu-buttons"><a href="#GroupageCargo">Сборные</a></div>
+                                                <div className="menu-buttons"><a href="#Main">Страхование</a></div>
+                                                <div className="menu-buttons"><a href="#Main">Таможеные услуги</a></div>
+                                            </div> */}
+                                        </div>
+                                    </div>
+                                    <div className="col-w25"></div>
+                                    <div className="col-w25"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </header>
