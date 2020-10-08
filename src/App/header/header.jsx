2bbfab0c3fbe0aee9184c49a17db49f5f9e8c767
e-marketing AntonB <u1-278@office107.local>
2020-10-08 17:40:24 +0300

    change header, added about us

--- src/App/header/header.jsx
+++ src/App/header/header.jsx
@@ -4,39 +4,44 @@ const Header = () => {
     return (
         <header>
             <div className="inline-wrapper">
-                <div className="col-w20">
-                    <div className="logo-container">
-                        <a className="logo" href="/"></a>
-                    </div>
-                </div>
-                <div className="col-w80">
-                    <div className="col-h50 contacts-container-main">
-                        <div className="contacts-container">
-                            <div className="col-h50 top-contacts-container">
-                                <div className="contacts">
-                                    <div className="col-h50">
-                                        <div className="phones">
-                                            <p>+380501523665</p>
-                                            <p>+380978757141</p>
-                                            <p>+380509898041</p>
-                                        </div>
-                                    </div>
-                                    <div className="col-h50">
-                                        <div className="email">
-                                            <a href="https://mail.ukr.net/desktop#sendmsg">logistics-solutions@ukr.net</a>
-                                            <a href="https://www.facebook.com/companylogisticssolutions/">Facebook</a>
+                <div className="inline-header">
+                    <div className="col-h50 top-header">
+                        <div className="col-w10">
+                            <div className="logo-container">
+                                <a className="logo" href="/"></a>
+                            </div>
+                        </div>
+                        <div className="col-w90">
+                            <div className="col-h70">
+                                <div className="contacts-container">
+                                    <div className="top-contacts-container">
+                                        <div className="contacts">
+                                            <div className="col-h50">
+                                                <div className="phones">
+                                                    <p>+380501523665</p>
+                                                    <p>+380978757141</p>
+                                                    <p>+380509898041</p>
+                                                </div>
+                                            </div>
+                                            <div className="col-h50">
+                                                <div className="email">
+                                                    <a href="https://mail.ukr.net/desktop#sendmsg">logistics-solutions@ukr.net</a>
+                                                    <a href="https://www.facebook.com/companylogisticssolutions/">Facebook</a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-h50 bottom-contacts-container"></div>
+                            <div className="col-h30 logo-text">
+                                <p>OGISTICS SOLUTIONS</p>
+                            </div>
                         </div>
                     </div>
                     <div className="col-h50">
                         <div className="navigations">
                             <div className="navigation-container">
-                                <div className="col-h30"></div>
-                                <div className="col-h70 menu-container">
+                                <div className="h100 menu-container">
                                     <div className="col-w19">
                                         <div className="menu-buttons"><a href="#Main">Главная</a></div>
                                     </div>
--- src/App/main/main.jsx
+++ src/App/main/main.jsx
@@ -3,10 +3,17 @@ import Slide from './slider';
 const Main = () => {
 return (
     <main>
-        <section className="about-company-info-container">
-            
-        </section>
         <Slide/>
+        <section className="about-company-info-container">
+            <div className="inline-wrapper">
+                <div className="about-us">
+                    <p>
+                    Компания <span>Logistics Solutions</span>, является одной из лучшей в предоставлении услуг грузоперевозок в Украине и за её пределами. Мы предоставляем услуги нашим клиентов, соблюдая все мировые стандарты качества. У нас работают только высококвалифицированные специалисты, которые курируют перевозки в отдаленные уголки мира.Выбирая нашу компанию, вы получаете гарантию надежности и качества оказываемых услуг. Мы строим Партнерские отношения в бизнесе, потому что для нас самое главное – ваше доверие.
+                    </p>
+                </div>
+            </div>
+        </section>
+        
     </main>
 );
 }
