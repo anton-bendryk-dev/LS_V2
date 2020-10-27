d7488cf4b82852dacdbee62c5bfc751baf24baee
anton bendryk <antonbendrik1993@gmail.com>
2020-10-27 15:42:20 +0200

    added about company info

---
+++ src/App/main/aboutCompany/aboutCompany.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutCompany = () => {
+return (
+    <section className="about-company-info-container">
+            <div className="bg-about-company-info-container h100">
+                <div className="inline-wrapper">
+                    <div className="about-us">
+                        <p>
+                        Компания <span>Logistics Solutions</span>, является одной из лучшей в предоставлении услуг грузоперевозок в Украине и за её пределами. Мы предоставляем услуги нашим клиентов, соблюдая все мировые стандарты качества. У нас работают только высококвалифицированные специалисты, которые курируют перевозки в отдаленные уголки мира.Выбирая нашу компанию, вы получаете гарантию надежности и качества оказываемых услуг. Мы строим Партнерские отношения в бизнесе, потому что для нас самое главное – ваше доверие.
+                        </p>
+                    </div>
+                </div>
+            </div>
+    </section>
+);
+}
+
+export default AboutCompany;
--- src/App/main/main.jsx
+++ src/App/main/main.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import Slider from './slider/slider';
 import Services from './services/services';
+import AboutCompany from './aboutCompany/aboutCompany';
 const Main = () => {
 return (
     <main>
         <Slider/>
-        <section className="about-company-info-container">
-            <div className="bg-about-company-info-container h100">
-                <div className="inline-wrapper">
-                    <div className="about-us">
-                        <p>
-                        Компания <span>Logistics Solutions</span>, является одной из лучшей в предоставлении услуг грузоперевозок в Украине и за её пределами. Мы предоставляем услуги нашим клиентов, соблюдая все мировые стандарты качества. У нас работают только высококвалифицированные специалисты, которые курируют перевозки в отдаленные уголки мира.Выбирая нашу компанию, вы получаете гарантию надежности и качества оказываемых услуг. Мы строим Партнерские отношения в бизнесе, потому что для нас самое главное – ваше доверие.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <AboutCompany/>
         <Services/>
     </main>
 );
