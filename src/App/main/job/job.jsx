eea0327dc1f34bd7204d4ed114c750876136388d
anton bendryk <antonbendrik1993@gmail.com>
2020-11-04 16:03:23 +0200

    added job

---
+++ src/App/main/job/job.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+const Job = () => {
+    return (
+        <div className="inline-wrapper job">
+            <div className="job-container">
+                <h3>Карьера в Logistics Solutions</h3>
+                <div className="job-info">
+                    <div className="job-info-text">
+                        <p className="job-description">Logistics Solutions - создает сильную команду единомышленников с единой корпоративной культурой. Постоянное развитие и совершенствование, командный дух, инновационность, инициативность и проактивность – фундаментальные элементы корпоративной культуры компании.</p>
+                    </div>
+                    <div className="job-info-text">
+                        <p>РАБОТА В КОМАНДЕ ПРОФЕССИОНАЛОВ</p>
+                        <p>КОМФОРТНЫЕ УСЛОВИЯ ТРУДА</p>
+                        <p>ВОЗМОЖНОСТЬ СТАЖИРОВКИ</p>
+                        <p>СОЦИАЛЬНЫЕ ГАРАНТИИ</p>
+                        <p>СТАБИЛЬНАЯ РАБОТА</p>
+                        <p>ДОСТОЙНАЯ ОПЛАТА</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Job
--- src/App/main/main.jsx
+++ src/App/main/main.jsx
@@ -3,7 +3,9 @@ import Slider from './slider/slider';
 import Services from './services/services';
 import AboutCompany from './aboutCompany/aboutCompany';
 import OurAdvantages from './ourAdvantages/ourAdvantages';
-import HowItWork from './howItWork/howItWork'
+import HowItWork from './howItWork/howItWork';
+import Job from './job/job';
+
 const Main = () => {
 return (
     <main>
@@ -12,6 +14,7 @@ return (
         <Services/>
         <OurAdvantages/>
         <HowItWork/>
+        <Job/>
     </main>
 );
 }
